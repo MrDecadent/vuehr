e78f1a05ae774c71ed3754e1eacf0e30f6ffe52e
MrDecadent <mrdecadent1314@gmail.com>
2022-06-23 00:57:36 +0800

    职位前后端接口对接

--- src/components/sys/basic/PosMana.vue
+++ src/components/sys/basic/PosMana.vue
@@ -6,16 +6,22 @@
                 class="addPosInput"
                 placeholder="添加职位..."
                 prefix-icon="el-icon-plus"
+                @keydown.enter.native="addPosition"
                 v-model="pos.name">
             </el-input>
-            <el-button icon="el-icon-plus" size="small" type="primary">添加</el-button>
+            <el-button icon="el-icon-plus" size="small" type="primary" @click="addPosition">添加</el-button>
         </div>
         <div class="posManaMain">
             <el-table
-                :data="positons"
+                :data="positions"
+                border
                 stripe
                 size="small"
                 style="width: 70%">
+                <el-table-column
+                    type="selection"
+                    width="55">
+                </el-table-column>
                 <el-table-column
                     prop="id"
                     label="编号"
@@ -27,8 +33,23 @@
                     width="120">
                 </el-table-column>
                 <el-table-column
-                    prop="createDate"
+                    prop="createdate"
                     label="创建时间">
+                </el-table-column>
+                <el-table-column
+                    label="操作">
+                    <template slot-scope="scope">
+                        <!-- scope.$index第几行的数据 scope.row数据(JSON) -->
+                        <el-button
+                                size="mini"
+                                @click="showEditView(scope.$index, scope.row)">编辑
+                        </el-button>
+                        <el-button
+                                size="mini"
+                                type="danger"
+                                @click="handleDelete(scope.$index, scope.row)">删除
+                        </el-button>
+                    </template>
                 </el-table-column>
             </el-table>
         </div>
@@ -36,6 +57,12 @@
 </template>
 
 <script>
+import { postRequest } from "@/utils/api"
+
+import { Message } from "element-ui"
+
+import { getRequest } from "@/utils/api"
+
     export default {
         name: "PosMana",
         data(){
@@ -43,7 +70,53 @@
                 pos:{
                     name:''
                 },
-                positons: []
+                positions: []
+            }
+        },
+        mounted(){
+            this.initPositions();
+        },
+        methods:{
+            showEditView(index,data){
+
+            },
+            handleDelete(index,data){
+                this.$confirm('此操作将永久删除【' + data.name + '】职位, 是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    console.log("del");
+                    this.deleteRequest("/system/basic/pos/" + data.id).then(resp => {
+                        if (resp) {
+                            this.initPositions();
+                        }
+                    })
+                }).catch(() => {
+                    this.$message({
+                        type: 'info',
+                        message: '已取消删除'
+                    });
+                });
+            },
+            addPosition(){
+                if(this.pos.name){
+                    this.postRequest("/system/basic/pos/",this.pos).then(resp=>{
+                        if(resp){
+                            this.initPositions();
+                            this.pos.name = '';
+                        }
+                    })
+                }else{
+                    this.$message.error('职位名称不能为空')
+                }
+            },
+            initPositions(){
+                this.getRequest("/system/basic/pos/").then(resp => {
+                    if (resp) {
+                        this.positions = resp;
+                    }
+                })
             }
         }
     }
