684ada68bc35391b4fa14e19045c7e7d6c8fd0ba
MrDecadent <mrdecadent1314@gmail.com>
2022-07-10 14:10:02 +0800

    实现添加员工界面的数据校验

--- src/views/emp/EmpBasic.vue
+++ src/views/emp/EmpBasic.vue
@@ -206,7 +206,7 @@
             <div style="display: flex;justify-content: flex-end">
                 <el-pagination
                     :page-size="10"
-                    :pager-count="6"
+                    :pager-count="7"
                     @current-change="currentChange"
                     @size-change="sizeChange"
                     layout="sizes, prev, pager, next, jumper, ->, total, slot"
@@ -219,7 +219,7 @@
             :visible.sync="dialogVisible"
             width="80%">
             <div>
-                <el-form>
+                <el-form :model="emp" :rules="rules" ref="empForm">
                     <el-row>
                         <el-col :span="6">
                             <el-form-item label="姓名:" prop="name">
@@ -274,7 +274,7 @@
                             </el-form-item>
                         </el-col>
                         <el-col :span="5">
-                            <el-form-item label="籍贯:" prop="nativePlace">
+                            <el-form-item label="籍贯:" prop="nativeplace">
                                 <el-input size="mini" style="width: 120px" prefix-icon="el-icon-edit"
                                           v-model="emp.nativeplace" placeholder="请输入籍贯"></el-input>
                             </el-form-item>
@@ -487,7 +487,7 @@
                     email: "laowang@qq.com",
                     phone: "18565558897",
                     address: "广州市天河区",
-                    departmentid: 5,
+                    departmentid: null,
                     joblevelid: 9,
                     posid: 29,
                     engageform: "劳务合同",
@@ -517,6 +517,44 @@
                     label: 'name'
                 },
                 inputDepName:'',
+                rules:{
+                    name:[{required: true,message: '请输入姓名' ,trigger: 'blur'}],
+                    gender: [{required: true, message: '请输入性别', trigger: 'blur'}],
+                    birthday: [{required: true, message: '请输入出生日期', trigger: 'blur'}],
+                    idCard: [{required: true, message: '请输入身份证号码', trigger: 'blur'}, {
+                        pattern: /(^[1-9]\d{5}(18|19|([23]\d))\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{3}[0-9Xx]$)|(^[1-9]\d{5}\d{2}((0[1-9])|(10|11|12))(([0-2][1-9])|10|20|30|31)\d{2}$)/,
+                        message: '身份证号码格式不正确',
+                        trigger: 'blur'
+                    }],
+                    wedlock: [{required: true, message: '请输入婚姻状况', trigger: 'blur'}],
+                    nationid: [{required: true, message: '请输入您组', trigger: 'blur'}],
+                    nativeplace: [{required: true, message: '请输入籍贯', trigger: 'blur'}],
+                    politicid: [{required: true, message: '请输入政治面貌', trigger: 'blur'}],
+                    email: [{required: true, message: '请输入邮箱地址', trigger: 'blur'}, {
+                        type: 'email',
+                        message: '邮箱格式不正确',
+                        trigger: 'blur'
+                    }],
+                    phone: [{required: true, message: '请输入电话号码', trigger: 'blur'}],
+                    address: [{required: true, message: '请输入员工地址', trigger: 'blur'}],
+                    departmentid: [{required: true, message: '请输入部门名称', trigger: 'blur'}],
+                    joblevelid: [{required: true, message: '请输入职称', trigger: 'blur'}],
+                    posid: [{required: true, message: '请输入职位', trigger: 'blur'}],
+                    engageform: [{required: true, message: '请输入聘用形式', trigger: 'blur'}],
+                    tiptopdegree: [{required: true, message: '请输入学历', trigger: 'blur'}],
+                    specialty: [{required: true, message: '请输入专业', trigger: 'blur'}],
+                    school: [{required: true, message: '请输入毕业院校', trigger: 'blur'}],
+                    begindate: [{required: true, message: '请输入入职日期', trigger: 'blur'}],
+                    workState: [{required: true, message: '请输入工作状态', trigger: 'blur'}],
+                    workid: [{required: true, message: '请输入工号', trigger: 'blur'}],
+                    contractterm: [{required: true, message: '请输入合同期限', trigger: 'blur'}],
+                    conversiontime: [{required: true, message: '请输入转正日期', trigger: 'blur'}],
+                    notworkdate: [{required: true, message: '请输入离职日期', trigger: 'blur'}],
+                    begincontract: [{required: true, message: '请输入合同起始日期', trigger: 'blur'}],
+                    endcontract: [{required: true, message: '请输入合同结束日期', trigger: 'blur'}],
+                    workage: [{required: true, message: '请输入工龄', trigger: 'blur'}],
+                },
+                showAdvanceSearchView: false,
             }
         },
         mounted(){
@@ -526,13 +564,17 @@
         },
         methods:{
             doAddEmp(){
-                this.postRequest("/emp/basic/",this.emp).then(result => {
-                    console.log(this.emp);
-                    if(result){
-                        this.dialogVisible = false;
-                        this.initEmps();
+                this.$refs['empForm'].validate(valid=>{
+                    if(valid){
+                        this.postRequest("/emp/basic/",this.emp).then(result => {
+                            console.log(this.emp);
+                            if(result){
+                                this.dialogVisible = false;
+                                this.initEmps();
+                            }
+                        });
                     }
-                });
+                })
             },
             handleNodeClick(data){
                 this.inputDepName = data.name;
